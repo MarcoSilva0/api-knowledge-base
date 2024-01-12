aa0fb99ce0932ef9100b659f909705b303ca37a5
MarcoSilva0 <marcoantonio02016@outlook.com>
2024-01-12 14:06:49 -0300

    feat: Add routes api to user CRUD

--- api/user.js
+++ api/user.js
@@ -1,7 +1,100 @@
+const bcrypt = require("bcrypt-nodejs");
+
 module.exports = (app) => {
-  const save = (req, res) => {
-    res.send("User save");
+  const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation;
+
+  const encryptPassword = (password) => {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
   };
 
-  return { save };
+  const save = async (req, res) => {
+    const user = { ...req.body };
+    if (req.params.id) user.id = req.params.id;
+
+    try {
+      existsOrError(user.name, "O dado name não informado");
+      existsOrError(user.email, "O dado email não informado");
+      existsOrError(user.password, "O dado password não informada");
+      existsOrError(user.confirmPassword, "O dado confirmPassword é inválido");
+      equalsOrError(user.password, user.confirmPassword, "Senhas não conferem");
+
+      const userFromDB = await app
+        .db("users")
+        .where({ email: user.email })
+        .first();
+      if (!user.id) {
+        notExistsOrError(userFromDB, "Usuário já cadastrado");
+      }
+    } catch (msg) {
+      return res.status(400).send(msg);
+    }
+
+    user.password = encryptPassword(user.password);
+    delete user.confirmPassword;
+
+    if (user.id) {
+      app
+        .db("users")
+        .update(user)
+        .where({ id: user.id })
+        .then((_) =>
+          res.status(200).send({
+            msg: "User successfully updated",
+            data: user,
+          })
+        )
+        .catch((err) => res.status(500).send(err));
+    } else {
+      app
+        .db("users")
+        .insert(user)
+        .then((_) =>
+          res.status(201).send({
+            msg: "User successfully created",
+            data: user,
+          })
+        )
+        .catch((err) => res.status(500).send(err));
+    }
+  };
+
+  const get = (req, res) => {
+    app
+      .db("users")
+      .select("id", "name", "email", "admin")
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).send(err));
+  };
+
+  const getById = (req, res) => {
+    app
+      .db("users")
+      .select("id", "name", "email", "admin")
+      .where({ id: req.params.id })
+      .first()
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).send(err));
+  };
+
+  const remove = async (req, res) => {
+    try {
+      const articles = await app
+        .db("articles")
+        .where({ userId: req.params.id });
+      notExistsOrError(articles, "Usuário possui artigos.");
+
+      const rowsUpdated = await app
+        .db("users")
+        .where({ id: req.params.id })
+        .del();
+      existsOrError(rowsUpdated, "Usuário não foi encontrado.");
+
+      res.status(200).send({ msg: "User deleted successfully.", data: null });
+    } catch (msg) {
+      res.status(400).send(msg);
+    }
+  };
+
+  return { save, get, getById, remove };
 };
--- readme.md
+++ readme.md
@@ -1,1 +1,26 @@
 docker-compose up -d
+
+## Erro no desenvolvimento e suas correções
+Erro: knex não é um comando reconhecido
+Solução:
+Instalar o knex globalmente.
+
+Comando: npm i -g knex
+
+----------------------------------------------------------------
+
+Erro knex init: No such file or directory
+Solução:
+Basta você ir na pasta do projeto > versao-inicial > backend > node_modules > knex > lib
+
+Quando chegar aí, crie uma nova pasta com o nome migrations e depois mova a pasta migrate para dentro dela.
+
+----------------------------------------------------------------
+
+Erro Knex migrate:make create_table_users: Cannot find module '../migrate'
+Solução:
+Acessar a pasta node_modules (versao-inicial/backend/node_modules), dentro dela achar a pasta knex/lib/util e abrir o arquivo make-knex.js
+
+Achar a linha referente ao migrate: var _migrate e corrigir para var _migrate = require('../migrations/migrate');
+
+Rodar o comando (create_table_users) novamente.
