14fb43c5c47245baa6cf944234ac2b6d0cf0ffd2
MarcoSilva0 <marcoantonio02016@outlook.com>
2024-01-12 14:15:22 -0300

    feat: Add category API

--- config/routes.js
+++ config/routes.js
@@ -6,4 +6,23 @@ module.exports = (app) => {
     .put(app.api.user.save)
     .get(app.api.user.getById)
     .delete(app.api.user.remove);
+
+  app
+    .route("/categories")
+    .all(app.config.passport.authenticate())
+    .get(admin(app.api.category.get))
+    .post(admin(app.api.category.save));
+
+  // Cuidado com ordem! Tem que vir antes de /categories/:id
+  app
+    .route("/categories/tree")
+    .all(app.config.passport.authenticate())
+    .get(app.api.category.getTree);
+
+  app
+    .route("/categories/:id")
+    .all(app.config.passport.authenticate())
+    .get(app.api.category.getById)
+    .put(admin(app.api.category.save))
+    .delete(admin(app.api.category.remove));
 };
