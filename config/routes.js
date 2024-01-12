d933f78f825173daa4fbfecaa95ff36f7cf3c07e
MarcoSilva0 <marcoantonio02016@outlook.com>
2024-01-12 14:19:28 -0300

    feat: Add articles routes

--- config/routes.js
+++ config/routes.js
@@ -25,4 +25,22 @@ module.exports = (app) => {
     .get(app.api.category.getById)
     .put(admin(app.api.category.save))
     .delete(admin(app.api.category.remove));
+
+  app
+    .route("/articles")
+    .all(app.config.passport.authenticate())
+    .get(admin(app.api.article.get))
+    .post(admin(app.api.article.save));
+
+  app
+    .route("/articles/:id")
+    .all(app.config.passport.authenticate())
+    .get(app.api.article.getById)
+    .put(admin(app.api.article.save))
+    .delete(admin(app.api.article.remove));
+
+  app
+    .route("/categories/:id/articles")
+    .all(app.config.passport.authenticate())
+    .get(app.api.article.getByCategory);
 };
